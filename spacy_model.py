4f9edeed2eefd265ba0f706c0e2aecc886183400
edoardo.ferrazzo <edoardo.ferrazzo@par-tec.it>
2023-11-30 12:27:46 +0100

    correzione errore it_sm

--- spacy_model.py
+++ spacy_model.py
@@ -11,8 +11,7 @@ import json
 #          'Torino FC', 'Torino', 'US Lecce', 'Lecce', 'AC Monza', 'Monza']
 
 def model_creation():
-    # nlp = spacy.load('it_core_news_lg')
-    nlp = spacy.blank('it_core_news_sm')
+    nlp = spacy.load('it_core_news_sm')
     team_names = json.load(open('data/teams_data.json'))['teams']
     add_labeled_entity_ruler_to_spacy_model(nlp, adjust_team_names_for_entity_ruler(team_names), 'TEAM')
     entity_ruler = nlp.get_pipe('entity_ruler')
